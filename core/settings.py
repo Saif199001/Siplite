ba43f267bb3c37b3fdaa059ad7503f1eb9064669
Saif199001 <zeeshasaif8@gmail.com>
2025-03-24 22:09:20 +0530

    Updated files and made necessary changes for runway domain

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ihx+iwvc3s*!y)j=hy@1=fz81@$g&m4#$o#6)vjl3*#k$=9xt$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.siplitenaturalteas.in']
 
 
 # Application definition
